de6e60f64cd69ba02181fe06ef86ce357ea0117e
Timothy Miamba <timal0361@gmail.com>
2023-11-15 10:52:17 +0200

    Construct todo module

---
+++ src/todo.js
@@ -0,0 +1,23 @@
+import logMessage from "./logger.js";
+
+export default function ToDo(title, description, dueDate, priority) {
+    let note = ''
+
+    const addNote = (value) => {
+        if (note.length > 1000){
+            logMessage('Notes cannot contain more than 1000 characters')
+        }
+        note = value
+    }
+
+    const getNote = () => note
+
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        addNote,
+        getNote,
+    }
+}
